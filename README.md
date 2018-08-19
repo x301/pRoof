9bfc35cd682a1cd41f1abe7918e1b1e9c53dd62c
Sergey <ishellyp@gmail.com>
2018-08-19 18:47:20 +0300

    Create start template for lp

---
+++ README.md
@@ -0,0 +1,1 @@
+# Start template for Front end.
---
+++ webpack/sass.js
@@ -0,0 +1,13 @@
+module.exports = function(paths) {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.sass$/,
+          include: paths,
+          use: ["style-loader", "css-loader", "sass-loader?sourceMap"]
+        }
+      ]
+    }
+  };
+};
